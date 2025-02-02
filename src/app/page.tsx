35521f1980c32eff69d10f5c6419871742bdf132
keshawrk <116962069+keshawrk@users.noreply.github.com>
2025-02-02 23:35:49 +0530

    Added authentication through clerk and added middleware

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,18 @@
 import { Button } from "@/components/ui/button";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
+    <div className="m-10">   
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+
+      <SignedIn>
+        <UserButton/>
+      </SignedIn>
       
-      <Button> Sign In </Button>
     </div>
   );
 }
