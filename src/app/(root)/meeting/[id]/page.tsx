404b2c7177a3dbb65d2cb06752a8a6eee6c64d50
keshawrk <116962069+keshawrk@users.noreply.github.com>
2025-02-19 23:31:36 +0530

    Video calls setup with Stream Provider

---
+++ src/app/(root)/meeting/[id]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MeetingPage = () => {
+  return (
+    <div>
+      page
+    </div>
+  )
+}
+
+export default MeetingPage
---
+++ src/app/(root)/recordings/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function RecordingsPage() {
+  return (
+    <div>
+      Recordings
+    </div>
+  )
+}
+
+export default RecordingsPage
